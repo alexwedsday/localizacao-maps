9e1ade702c0046755aa730981e474dd82818bc43
Alex Wedsday <alexwedsday@hotmail.com>
2022-07-16 09:34:28 -0300

    Setup Angular with PrimeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
+    ButtonModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
